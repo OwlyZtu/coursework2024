9978533d07d04a7211ce1030c5583f0ecb2113cc
Sofiia <vt211_pso@student.ztu.edu.ua>
2024-05-24 18:46:44 +0300

    fix nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -51,5 +51,5 @@ disableLogger: true,
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+automaticVercelMonitors: false,
 });
